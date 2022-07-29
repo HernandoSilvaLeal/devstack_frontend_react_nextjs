dd8e83857a17eb2eee6b1200fad46582366139a3
Hernando Silva Leal <hernandosilvaleal@gmail.com>
2022-07-29 14:42:09 -0500

    Errores de compatibilidad con react router V6 solucionados

--- blogapi/src/index.js
+++ blogapi/src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Register from './components/Register';
+import Login from './components/Login';
+import Logout from './components/Logout';
 
 const routing = (
 	<Router>
 		<React.StrictMode>
 			<Header />
-			<Switch>
-				<Route exact path="/" component={App} />
-			</Switch>
+			<Routes>
+				<Route exact path="/" element={App} />
+				<Route path="/register" element={Register} />
+				<Route path="/login" element={Login} />
+				<Route path="/logout" element={Logout} />
+			</Routes>
 			<Footer />
 		</React.StrictMode>
 	</Router>
